65e080bc0c1e27f5b650f7486b813a3f5ef89c3e
tanmay mishra <tanmaymishra35@gmail.com>
2020-07-24 11:53:22 +0530

    heroku deployed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import path from 'path'
 import data from './data';
-import dotenv from 'dotenv';
+
 import config from './config';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
