50f4d5c60bfe4fd49d7903991257cdd05dd0b63f
tanmay mishra <tanmaymishra35@gmail.com>
2020-07-24 09:51:35 +0530

    Project Completed

---
+++ backend/data.js
@@ -0,0 +1,49 @@
+export default  {
+  products : [{
+      _id: '1',
+      name : "Samsung Galaxy S20",
+      category: "Mobile",
+      image : "/images/s20.jpg",
+      price : 70499,
+      ratings : 4.5,
+      numReviews: 80,
+      countInStock: 6,
+      brand: 'Samsung'
+  },
+  {
+      _id: '2',
+      name : "Redmi Note 9",
+      category: "Mobile",
+      image : "/images/redmi9.png",
+      price : 12499,
+      ratings : 4.5,
+      numReviews: 80,
+      brand: 'Xiaomi'
+  },
+  
+  {
+      _id: '3',
+      name : "Apple Iphone 11",
+      category: "Mobile",
+      image : "/images/iphone11.jpg",
+      price : 73499,
+      ratings : 4.6,
+      numReviews: 80,
+      countInStock: 6,
+      brand: 'Apple'
+  },
+  {
+      _id: '4',
+      name : "Apple Macbook Pro",
+      category: "Laptop",
+      image : "/images/macbookpro.jpg",
+      price : 52499,        
+      ratings : 4.6,
+      numReviews: 80,
+      countInStock: 6,
+      brand: 'Apple'
+  },
+
+
+]
+}
